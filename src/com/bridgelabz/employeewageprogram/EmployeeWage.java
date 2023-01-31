072467730abc1986e02f1a93fe14d6a97ddf3eb0
deepeshpradhan <dpradhan0610@gmail.com>
2023-02-01 00:49:05 +0530

    Added: Solving program using Swtich Case

--- src/com/bridgelabz/employeewageprogram/EmployeeWage.java
+++ src/com/bridgelabz/employeewageprogram/EmployeeWage.java
@@ -6,19 +6,22 @@ public class EmployeeWage {
     static int Wage_per_Hour = 20;
     static int Full_Day_Hour = 8;
     static int Part_Time_Hour = 4;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         Random ran = new Random();
         int attendance = ran.nextInt(3);
 
-        if (attendance == 1) {
-            System.out.println("Employee is Present for Full time");
-            System.out.println("Employee`s Daily Wage is => "+Wage_per_Hour * Full_Day_Hour);
-        } else if (attendance == 2) {
-            System.out.println("Employee is Present for Part time");
-            System.out.println("Employee`s Daily Wage is => "+Wage_per_Hour * Part_Time_Hour);
-        } else {
-            System.out.println("Employee is Absent");
+        switch (attendance) {
+            case 1 -> {
+                System.out.println("Employee is Present for Full time");
+                System.out.println("Employee`s Daily Wage is => " + Wage_per_Hour * Full_Day_Hour);
+            }
+            case 2 -> {
+                System.out.println("Employee is Present for Part time");
+                System.out.println("Employee`s Daily Wage is => " + Wage_per_Hour * Part_Time_Hour);
+            }
+            default -> System.out.println("Employee is Absent");
         }
     }
 }
