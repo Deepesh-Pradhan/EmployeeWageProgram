a5ab9075d92d42e115492ce4059d20bf9acf5896
deepeshpradhan <dpradhan0610@gmail.com>
2023-02-03 21:20:28 +0530

    Added: Store the daily wage along with total wage

--- src/com/bridgelabz/employeewageprogram/CompanyEmployeeWage.java
+++ src/com/bridgelabz/employeewageprogram/CompanyEmployeeWage.java
@@ -1,15 +1,20 @@
 package com.bridgelabz.employeewageprogram;
+
 interface InterfaceCalculateEmployeeWage {
     void companyEmployeeWages(String companyName, int wagePerHour, int totalWorkingDaysPerMonth, int totalWorkingHoursPerMonth);
+
     void calculateEmployeeWage();
+
     int getTotalWage(String companyName);
 }
+
 public class CompanyEmployeeWage {
     public final String companyName;
     public final int wagePerHour;
     public final int totalWorkingDaysPerMonth;
     public final int totalWorkingHoursPerMonth;
     public int totalEmployeeWage;
+
     public CompanyEmployeeWage(String companyName, int wagePerHour, int totalWorkingDaysPerMonth, int totalWorkingHoursPerMonth) {
         this.companyName = companyName;
         this.wagePerHour = wagePerHour;
@@ -17,9 +22,11 @@ public class CompanyEmployeeWage {
         this.totalWorkingHoursPerMonth = totalWorkingHoursPerMonth;
         totalEmployeeWage = 0;
     }
-    public void setTotalEmployeeWage(int totalEmployeeWage){
+
+    public void setTotalEmployeeWage(int totalEmployeeWage) {
         this.totalEmployeeWage = totalEmployeeWage;
     }
+
     @Override
     public String toString() {
         return "Total Employee Wage for " + companyName + " is => " + totalEmployeeWage;
